79adb2be77675edd8bd1f4251d1ab611db962205
adam-liao <adam.liao@gmail.com>
2025-05-28 10:53:33 +0800

    update

---
+++ code/js3-3/my.js
@@ -0,0 +1,13 @@
+function xxx() {
+    let pic = document.getElementById("pic");
+    pic.src = "./img/kiwi3.jpg"
+
+}
+
+// document.getElementById("btn").onclick=xxx
+
+// 5 sec 後換圖
+setTimeout(xxx, 5000)
+
+set
+
