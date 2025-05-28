32fea2b773b34fcca4832976c60a23e8bbc77f36
adam-liao <adam.liao@gmail.com>
2025-05-28 13:24:44 +0800

    update

--- code/JAV05-4-設定選購商品數量-遞增遞減/my.js
+++ code/JAV05-4-設定選購商品數量-遞增遞減/my.js
@@ -1,3 +1,19 @@
 
+let N = 12
 
+document.getElementById('minus').onclick = aaa
+document.getElementById('add').onclick = bbb
+
+
+// Num.innerHTML = N
+
+function aaa() {
+    N = N1 - 1
+    document.getElementById('num').innerHTML = N
+}
+function bbb() {
+    N = N + 1
+    document.getElementById('num').innerHTML = N
+
+}
 
