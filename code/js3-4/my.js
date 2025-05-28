d92d702d98a0da03ec4e1ff4296e17417f30cee5
adam-liao <adam.liao@gmail.com>
2025-05-28 11:46:12 +0800

    update

--- code/js3-4/my.js
+++ code/js3-4/my.js
@@ -1,15 +1,33 @@
 
+let pic = document.getElementById("pic");
+let tt = document.getElementById("tt");
 function xxx() {
-    let pic = document.getElementById("pic");
-    document.getElementById("tt").innerHTML = "芥末綠"
+    tt.innerHTML = "芥末綠"
     pic.src = "./img/g01.jpg"
 
 }
 function yyy() {
-    let pic = document.getElementById("pic");
     pic.src = "./img/g02.jpg"
-    document.getElementById("tt").innerHTML = "芥末粉"
+    tt.innerHTML = "芥末粉"
 }
 
 document.getElementById("icon1").onclick = xxx
 document.getElementById("icon2").onclick = yyy
+
+
+
+
+// function xxx() {
+//     let pic = document.getElementById("pic");
+//     document.getElementById("tt").innerHTML = "芥末綠"
+//     pic.src = "./img/g01.jpg"
+
+// }
+// function yyy() {
+//     let pic = document.getElementById("pic");
+//     pic.src = "./img/g02.jpg"
+//     document.getElementById("tt").innerHTML = "芥末粉"
+// }
+
+// document.getElementById("icon1").onclick = xxx
+// document.getElementById("icon2").onclick = yyy
