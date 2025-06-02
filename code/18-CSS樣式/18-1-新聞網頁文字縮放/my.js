5e6d86d32b5a649768035c305d2247b0129e0479
adam-liao <adam.liao@gmail.com>
2025-06-02 13:34:22 +0800

    update

--- code/18-CSS樣式/18-1-新聞網頁文字縮放/my.js
+++ code/18-CSS樣式/18-1-新聞網頁文字縮放/my.js
@@ -1,1 +1,19 @@
-document.getElementById
+document.getElementById("iconL").onclick = function (e) {
+    aaa(30);
+    e.preventDefault();
+}
+
+document.getElementById("iconM").onclick = function (e) {
+        aaa(16);
+    e.preventDefault();
+}
+document.getElementById("iconS").onclick = function (e){
+        aaa(12);
+        e.preventDefault();
+
+}
+
+
+function aaa(N) {
+    document.getElementById("tt").style.fontSize = N + "px"
+}
