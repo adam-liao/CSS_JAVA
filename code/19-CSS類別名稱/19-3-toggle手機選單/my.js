b47dfde1f4b941d08555a365580ff8c52f95ece2
adam-liao <adam.liao@gmail.com>
2025-06-02 15:39:44 +0800

    update

--- code/19-CSS類別名稱/19-3-toggle手機選單/my.js
+++ code/19-CSS類別名稱/19-3-toggle手機選單/my.js
@@ -1,3 +1,11 @@
+document.getElementById("icon").onclick = show;
 
+// function show() {
+//     document.getElementById("menu").classList.toggle("show-hide")
 
+// }
 
+document.getElementById("icon").onclick = show;
+function show() {
+     
+    document.getElementById("menu").classList.toggle("show-hide"); }
